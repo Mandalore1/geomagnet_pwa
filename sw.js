a2d7740adbe39e3c3d8f9acb248dd9c1c4be34e6
Mandalore <serriy_volk@mail.ru>
2023-02-05 16:34:29 +0500

    Service worker cache tiles

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const {
     cacheableResponse: { CacheableResponsePlugin },
     expiration: { ExpirationPlugin },
     routing: { registerRoute },
-    strategies: { CacheFirst },
+    strategies: { CacheFirst, StaleWhileRevalidate },
 } = workbox;
 
 registerRoute(
@@ -20,4 +20,25 @@ registerRoute(
             }),
         ],
     }),
+);
+
+const tilesUrls = ["https://ibasemaps-api.arcgis.com/arcgis/rest/", "https://basemaps-api.arcgis.com/arcgis/rest/",
+    "https://services.arcgisonline.com/ArcGIS/rest/"]
+
+registerRoute(
+    // Custom `matchCallback` function
+    ({event}) => event.request.url.startsWith(tilesUrls[0]) || event.request.url.startsWith(tilesUrls[1]) ||
+        event.request.url.startsWith(tilesUrls[2]),
+    new CacheFirst({
+        cacheName: 'tiles',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 200,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+            }),
+        ],
+    })
 );
